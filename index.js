0b4488b76da692f49a06665f4af750add7d8c585
Ali Noon <alinoonciit@gmail.com>
2020-06-16 22:08:27 +0500

    Fixed Bug

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ require("./routes/authRoutes")(app);
 require("./routes/billingRoutes")(app);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("/static/build"));
+  app.use(express.static("client/build"));
 } else {
   const path = require("path");
   app.get("*", (req, res) => {
